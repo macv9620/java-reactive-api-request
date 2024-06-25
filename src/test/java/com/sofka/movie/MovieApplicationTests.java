c0c836eb703d151c2fec09e10dddefb8b7e38626
macv9620 <116320354+macv9620@users.noreply.github.com>
2024-06-25 11:20:40 -0500

    Update MovieApplicationTests.java

--- src/test/java/com/sofka/movie/MovieApplicationTests.java
+++ src/test/java/com/sofka/movie/MovieApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MovieApplicationTests {
-
+class MovieApplicationConflictMainTests {
+//Comment to check from main
 	@Test
 	void contextLoads() {
 	}
